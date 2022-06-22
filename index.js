6e10a0263e588920bee570626c6d3e732b270d95
Bhavesh588 <sbhavesh588@gmail.com>
2022-06-22 09:15:05 +0530

    First Commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const path = require("path");
+// const path = require("path");
 
 const { sequelize, User, Posts } = require("./models");
 const app = express();
